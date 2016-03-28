7be2a40f62b377c8def0590e6ea57e6874b4760b
lucasprogamer <lucasouzaweb@hotmail.com>
2016-03-28 19:41:35 -0300

    First Commit

---
+++ Apoio_Gerenciamento_Ativos/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Apoio_Gerenciamento_Ativos.MvcApplication" Language="C#" %>
---
+++ Apoio_Gerenciamento_Ativos/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Apoio_Gerenciamento_Ativos.Startup))]
+namespace Apoio_Gerenciamento_Ativos
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
